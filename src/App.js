94a14f0900b4b5df7dda3d3df12ce792fd9fb65a
Carlos Ordaz <carlos.ordaz@acomo.com.pe>
2023-12-15 00:09:00 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import { HomePage } from './components/HomePage';
+
+
+
+function App() {
+  return (
+    <>
+      <Routes>
+       <Route exact path='/' element={<HomePage />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
